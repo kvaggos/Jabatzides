927cf61469cdfd757c14f8b8e087efc5deca4bef
kvaggos <44069528+kvaggos@users.noreply.github.com>
2018-11-02 16:28:09 +0200

    Update Table.java

--- Table.java
+++ Table.java
@@ -4,34 +4,25 @@ public class Table{
 
 	String name;
 
-	static ArrayList <Table> listoftables = new ArrayList <Table>();
-	static TreeMap<String, Object> table = new TreeMap<String, Object>();
+	static ArrayList <Table> listoftables = new ArrayList <Table>(); //arraylist which contains the tables
+	static TreeMap<String, Object> table = new TreeMap<String, Object>(); //TreeMap that contains the rows and the values of every table
 
 
-	public Table(String name){
+	public Table(String name){ //constructor
 		this.name = name;
-
-
 		}
 
-	public void addTolistoftables() {
-
+	public void addTolistoftables() { //add this table to the list of tables
 		listoftables.add(this);
 }
 
-
-      public boolean equals(Object o) {
+      public boolean equals(Object o) { // override the equals method
 
         if (o == this) return true;
         if (!(o instanceof Table)) {
             return false;
         }
-
         Table table = (Table) o;
-
         return table.name.equals(name);
-
-
 	}
-
 }
