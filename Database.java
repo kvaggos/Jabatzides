539a23711965113ca93b81f926c9621d63253c12
kvaggos <44069528+kvaggos@users.noreply.github.com>
2018-11-18 20:08:14 +0200

    Update Database.java

--- Database.java
+++ Database.java
@@ -1,98 +1,118 @@
 import java.util.*;
 
-  public class Database{
+public class Database {
 
- 	static Scanner sc = new Scanner(System.in);
+	static Scanner sc = new Scanner(System.in);
 
- 	public static void main(String[]args){
+	public static void main(String[] args) {
 
- 	boolean endcheck = true; // if false programm ends
+		boolean endcheck = true; // if false programm ends
 
- 	while(endcheck){
+		while (endcheck) {
 
- 	getMenu();	// call menu method
- 	int x = sc.nextInt();
+			getMenu(); // call menu method
+			int x = sc.nextInt();
 
+			if (x == 1) {
+				createTable();
+			} else if (x == 2) {
+				// addAttr();
+			} else if (x == 3) {
+				// viewTable();
+			} else if (x == 4) {
+				// deleteAttr();
+			} else if (x == 5) {
+				// modifyAttr();
+			} else if (x == 6) {
+				endcheck = false;
+				// }
+			}
+		}
+	}
 
- 	if 	(x == 1){
- 		createTable();
- 		 }else if (x == 2){
- 			//addAttr();
- 		}else if (x == 3){
- 			viewTable();
- 		}else if (x == 4){
- 			deleteAttr();
- 		}else if (x == 5){
- 			modifyAttr();
- 		}else if (x == 6){
- 			endcheck = false;
- 		//}
- 	}
- 	}
- }
+	public static void getMenu() {
+		System.out.println("****Welcome to our DATABASE****");
+		System.out.println("    Make a choice: ");
+		System.out.println("1. Create new table ");
+		System.out.println("2. Add attributes to a table ");
+		System.out.println("3. View from a table ");
+		System.out.println("4. Delete attributes from a table ");
+		System.out.println("5. Modify attributes of a table ");
+		System.out.println("6. End programm");
+	}
 
- 		public static void getMenu(){
- 			System.out.println("****Welcome to our DATABASE****");
- 			System.out.println("    Make a choice: ");
- 			System.out.println("1. Create new table ");
- 			System.out.println("2. Add attributes to a table ");
- 			System.out.println("3. View from a table ");
- 			System.out.println("4. Delete attributes from a table ");
- 			System.out.println("5. Modify attributes of a table ");
- 			System.out.println("6. End programm");
- 			}
+	public static void createTable() {
 
- 		public static void createTable(){
+		boolean checkexistingtable = true; // checks if the table name is available
+		Scanner sc = new Scanner(System.in);
+		Table tableobj = null; // creating Table type object
+		String name;
 
- 			boolean checkexistingtable = true; // checks if the table name is available
- 			Scanner sc = new Scanner(System.in);
- 			Table tableobj = null; //creating Table type object
- 			String name;
+		while (checkexistingtable) { // while checkeistingtable = true
 
- 			while(checkexistingtable){ //while checkeistingtable = true
+			System.out.println("Give table name");
+			name = sc.nextLine(); // read the table's name
 
- 				System.out.println("Give table name");
- 				name = sc.nextLine();	//read the table's name
+			if (tableobj.getTable(name) == null) { // checks if table names exists already
+				checkexistingtable = true; // make again the loop
+			} else {
+				tableobj = new Table(name); // the given name becomes a table name
+				tableobj.setTable(name); // add the table to the list of tables
+				checkexistingtable = false; // end the loop
+			}
+		}
 
- 				if (tableobj.getTable(name) == null){ //checks if table names exists already
- 					checkexistingtable = true; // make again the loop
- 					}else{
- 						 tableobj = new Table(name); //the given name becomes a table name
- 						 tableobj.setTable(name);	//add the table to the list of tables
- 						 checkexistingtable = false; // end the loop
- 					}
- 			}
+		boolean check1 = true; // checks if you want to stop adding rows and values
+		boolean check2 = true; // checks if you want to change row
 
+		while (check1) {
 
- 				boolean check1 = true; // checks if you want to stop adding rows and values
- 				boolean check2 = true; // checks if you want to change row
+			System.out.println("Give row name");
+			String rowname = sc.nextLine();
+			Object value; // takes every kind of a variable's type
 
+			LinkedList<Object> list = new LinkedList<Object>(); // list that contains the values of a row
+			check2 = true;
+			while (check2) { // while in this loop, values are added to a row
 
- 				while(check1){
+				System.out.println("Give input");
+				System.out.println("To end type: stop");
+				System.out.println("To change row type: change");
+				value = sc.nextLine();
+				if (value.equals("change")) { // change row
+					check2 = false;
+				} else if (value.equals("stop")) { // stop adding rows and values to the table
+					check2 = false;
+					check1 = false;
+				} else {
+					list.add(value); // add the given value to the list of values
+				}
 
- 				System.out.println("Give row name");
- 				String rowname = sc.nextLine();
- 				Object value; // takes every kind of a variable's type
+			}
+			tableobj.putValues(rowname, list); // save the rows and the lists(value's of every row) to TreeMap
+		}
+	}
 
- 				LinkedList <Object> list = new LinkedList <Object>(); //list that contains the values of a row
- 					check2 = true;
- 					while(check2){ // while in this loop, values are added to a row
+	public static void addAtt2Table() {
 
- 					System.out.println("Give input");
- 					System.out.println("To end type: stop");
- 					System.out.println("To change row type: change");
- 					value = sc.nextLine();
- 					if( value.equals("change")){ //change row
- 						check2 = false;
- 					}else if( value.equals("stop")){ // stop adding rows and values to the table
- 								check2 = false;
- 								check1 = false;
- 						}else{
- 					 list.add(value); // add the given value to the list of values
- 						}
+		Table myTable = new Table(); // creating Table type object
 
- 					}
- 					tableobj.putValues(rowname, list); // save the rows and the lists(value's of every row) to TreeMap
- 				}
- 		}
-  }
+		myTable = myTable.searchTable();
+
+		if (myTable == null) {
+			return;
+		}
+
+		Scanner s2 = new Scanner(System.in);
+		String resp = "o";
+		while (!(resp.equals("x"))) {
+			System.out.println("Add Rows (r) or Columns (c). Enter (x) to exit");
+			resp = s2.nextLine();
+			if (resp.equals("r")) {
+				myTable.addRows();
+			} else if (resp.equals("c")) {
+				myTable.addElements();
+			}
+		}
+	}
+}
