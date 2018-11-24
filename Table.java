8e3e39e11fdf1f1ec308ddfcefcd89b63454c25c
DimitrisPapavasileiou <44033686+DimitrisPapavasileiou@users.noreply.github.com>
2018-11-24 21:24:14 +0200

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,28 +1,123 @@
 import java.util.*;
+import java.util.Scanner;
+import java.util.regex.*;
 
-public class Table{
+public class Table {
 
 	String name;
+	Scanner sc = new Scanner(System.in);
 
+     Map<String, String> ColTypes = new HashMap<String, String>();
      static HashMap<String, Table> listoftables  = new HashMap<String, Table>();
-	//static ArrayList <Table> listoftables = new ArrayList <Table>(); //arraylist which contains the tables
-	 HashMap<String, LinkedList<Object> > table = new HashMap<String, LinkedList<Object>>(); //TreeMap that contains the rows and the values of every table
+	 Map<String, LinkedList<String> > table = new LinkedHashMap<String, LinkedList<String>>(); //TreeMap that contains the rows and the values of every table
 
 
 	public Table(String name){ //constructor
 		this.name = name;
-		}
+    }
 
-  public void putValues(String rowname, LinkedList<Object> values) {
+    public Table() {}
+
+    public void putValues(String rowname, LinkedList<String> values) {
 
 	   table.put(rowname, values);
+	}
+
+	      public void createTableCols() {
+                 boolean check1 = true; // checks if you want to stop adding rows and values
+			     boolean check2 = true; // checks if you want to change row
+				String coltype = "a";
+
+					LinkedList <String> list = new LinkedList <String>(); //list that contains the values of a row
+	  				System.out.println("Give column name or press enter to exit");
+	  				String colname = sc.nextLine();
+	  				check1 = !(colname == "");
+	  				while(check1)  {
+	  					System.out.println("Give column type (S)tring,(I)nt or (D)ouble");
+	  					while("SID".toLowerCase().indexOf(coltype.toLowerCase()) == -1 || coltype.equals("")) {
+	  						coltype = sc.nextLine();
+						}
+	  					ColTypes.put(colname, coltype);
+						putValues(colname, list);
+						System.out.println("Give next column name or press enter to exit");
+						colname = sc.nextLine();
+	  					check1 = !(colname == "");
+	  					coltype = "a";
+					}
+			}
 
 
-	  }
+
+		public  Table searchTable() {
+   			boolean workwithtable = true; // Work with a table
+   			Table tableobj = null; //Work table
+   			String name;
+   			System.out.println("Give the table's name to add attributes or EXIT to exit :");
+   			while(workwithtable){
+   				name = sc.nextLine();	//read the table's name
+
+   				if(name.equals("EXIT")) {
+   					workwithtable = false;
+   				} else {
+   					tableobj = getTable(name);
+   					if ( tableobj == null){
+   						System.out.println("Table " + name + " does not exists");
+   					} else {
+   						workwithtable = false;
+   					}
+   				}
+			}
+			return tableobj;
+		}
+
+
+	public void addRows() {
+		String myval="";
+		String coltyp = "";
+		String styp = "";
+		boolean morerow = true;
+		boolean datok = true;
+		while(morerow) {
+			for (String key : table.keySet()) {
+				coltyp = ColTypes.get(key);
+				switch (coltyp) {
+					case "S" : styp = "a String";
+					case "I" : styp = "an Integer";
+					case "D" : styp = "a Double";
+				}
+				System.out.println("Enter " + styp + " value for " + key);
+				myval = sc.nextLine();
+				while(checkval(myval, coltyp)) {
+					System.out.println("Enter a corect value ");
+					myval = sc.nextLine();
+				}
+				table.get(key).add(myval);
+			}
+			System.out.println("Add next row ?(Yes/No)");
+			myval = sc.nextLine();
+			morerow = (myval.equalsIgnoreCase("yes"));
+		}
+	}
+
+	public boolean checkval(String tval, String typeof) {
+		if (typeof == "S") {
+			return true;
+		}
+		//
+		if (typeof == "I" && tval.matches("-?[0-9]+")) {
+			return true;
+		}
+		if (typeof == "D" && tval.matches("\\d+\\.\\d+")) {
+			return true;
+		}
+		return false;
+	}
+
+
 
 	public void setTable(String name) {
 		listoftables.put(name, this);
-}
+   }
 
     public Table getTable(String name) {
 		if(listoftables.containsKey(name)) {
@@ -46,80 +141,33 @@ public class Table{
         Table table = (Table) o;
         return table.name.equals(name);
 	}
+
+
+	public void viewWholeTable(){
+
+
+
+			for (String key : table.keySet()){
+				System.out.print(key + "   ");
+				}
+			System.out.println();
+
+
+			int x = 0;
+
+			//for(Map.Entry<String, LinkedList<Object>> entry : table.entrySet()) {
+			//    System.out.println(entry.getKey() + ": " +  entry.getValue().toString());
+			//}
+			System.out.println("table size = " + table.size());
+			for(int i = 0;i<table.size();i++){
+
+				for (String key2 : table.keySet()){
+					System.out.print(table.get(key2).get(x) + "\t");
+				}
+				System.out.println();
+				x++;
+				}
+			}
+
+
 }
-
-	public void addRows() {
-		String myval;
-		boolean moreval = true;
-		for (String key : table.keySet()) {
-			System.out.println("Enter value for " + key + " or dot(.) to end");
-			myval = sc.nextLine();
-			moreval = !(myval.equals("."));
-			while(moreval) {
-				table.get(key).add(myval);
-				System.out.println("Enter next value for " + key + " or dot(.) to end");
-				myval = sc.nextLine();
-				moreval = !(myval.equals("."));
-			}
-		}
-	}
-      
-    
-		public  Table searchTable() {
-   			boolean workwithtable = true; // Work with a table
-   			Table tableobj = null; //Work table
-   			String name;
-   			System.out.println("Give the table's name to add attributes or EXIT to exit :");
-   			while(workwithtable){
-   				name = sc.nextLine();	//read the table's name
-
-   				if(name.equals("EXIT")) {
-   					workwithtable = false;
-   				} else {
-   					tableobj = getTable(name);
-   					if ( tableobj == null){
-   						System.out.println("Table " + name + " does not exists");
-   					} else {
-   						workwithtable = false;
-   					}
-   				}
-			}
-			return tableobj;
-		}
-
-            public void addElements() {
-                 boolean check1 = true; // checks if you want to stop adding rows and values
-		 boolean check2 = true; // checks if you want to change row
-                 LinkedList <Object> list = new LinkedList <Object>(); //list that contains the values of a row
-                 while(check1)  {
-
-	  				System.out.println("Give column name");
-	  				String rowname = sc.nextLine();
-	  				Object value; // takes every kind of a variable's type
-
-	  					check2 = true;
-	  					System.out.println("Give input");
-						System.out.println("To end type: stop");
-	  					System.out.println("To change row type: change");
-	  					while(check2){ // while in this loop, values are added to a row
-
-	  					  value = sc.nextLine();
-	  					  if( value.equals("change")){ //change row
-	  						check2 = false;
-	  					  }else if( value.equals("stop")){ // stop adding rows and values to the table
-	  							check2 = false;
-	  							check1 = false;
-	  						    }else{
-	  					          list.add(value); // add the given value to the list of values
-	  						    }
-
-	  					}
-	  					if( list.isEmpty()) {
-
-	  					putValues(rowname, null); // save the rows and the lists(value's of every row) to TreeMap
-	  				} else {
-	 				    putValues(rowname, list);
-	 				    list.clear();
- 		        }
-		  }
-	  }
