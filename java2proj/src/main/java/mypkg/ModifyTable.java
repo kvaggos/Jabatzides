6497499d3abe3d60278e03f97ae92fb780bf58b4
DimitrisPapavasileiou <44033686+DimitrisPapavasileiou@users.noreply.github.com>
2019-01-08 17:18:41 +0200

    Update ModifyTable.java

--- java2proj/src/main/java/mypkg/ModifyTable.java
+++ java2proj/src/main/java/mypkg/ModifyTable.java
@@ -51,7 +51,7 @@ public class ModifyTable {
 			System.out.println("Give the new name of the column");
 			String newname = sc.nextLine();
 			 while(newname.trim().isEmpty() || newname.equals(oldname)) {
-				System.out.println("Column name cant be an empty line. Please try again");
+				System.out.println("Column name cant be an empty line or the same as before. Please try again");
 				newname = sc.nextLine();
 		         }
 			table.renameColumn(oldname, newname);
